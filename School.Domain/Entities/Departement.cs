a60f9a4aa08fa62a2e588b82127f21c88c510295
abdellaheltrach <abdellah.eltrach@gmail.com>
2025-12-23 22:00:03 +0100

    Add project files.

---
+++ School.Domain/Entities/Departement.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace School.Domain.Entities
+{
+    public partial class Department 
+    {
+        public Department()
+        {
+            Students = new HashSet<Student>();
+            DepartmentSubjects = new HashSet<DepartmetSubject>();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+        public string? DNameAr { get; set; }
+        [StringLength(200)]
+        public string? DNameEn { get; set; }
+
+        public int? InsManager { get; set; }
+
+        [InverseProperty("Department")]
+        public virtual ICollection<Student> Students { get; set; }
+        [InverseProperty("Department")]
+        public virtual ICollection<DepartmetSubject> DepartmentSubjects { get; set; }
+        [InverseProperty("department")]
+
+ 
+
+    }
+}
---
+++ School.Domain/Entities/Subject.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace School.Domain.Entities
+{
+    public class Subjects 
+    {
+        public Subjects()
+        {
+            StudentsSubjects = new HashSet<StudentSubject>();
+            DepartmetsSubjects = new HashSet<DepartmetSubject>();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SubID { get; set; }
+        [StringLength(500)]
+        public string? SubjectNameAr { get; set; }
+        public string? SubjectNameEn { get; set; }
+        public int? Period { get; set; }
+        [InverseProperty("Subject")]
+        public virtual ICollection<StudentSubject> StudentsSubjects { get; set; }
+        [InverseProperty("Subject")]
+        public virtual ICollection<DepartmetSubject> DepartmetsSubjects { get; set; }
+
+    }
+}
