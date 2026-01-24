e3ea9aff2c4e4861763159083453a3e468e3898e
abdellaheltrach <abdellah.eltrach@gmail.com>
2026-01-25 00:30:25 +0100

    add Change User Password endpoint

---
+++ School.Core/Features/Users/Commands/Models/ChangeUserPasswordCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using School.Core.Base.ApiResponse;
+
+namespace School.Core.Features.Users.Commands.Models
+{
+    public class ChangeUserPasswordCommand : IRequest<ApiResponse<string>>
+    {
+        public int Id { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string NewPasswordConfirmation { get; set; }
+    }
+}
--- School.Domain/AppRoutes/AppRouter.cs
+++ School.Domain/AppRoutes/AppRouter.cs
@@ -37,6 +37,7 @@
             public const string Paginated = Prefix + "/Paginated";
             public const string Edit = Prefix + "/Edit";
             public const string Delete = Prefix + "/{Id}";
+            public const string ChangePassword = Prefix + "/ChangePassword";
 
 
         }
