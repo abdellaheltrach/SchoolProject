c3b3c99731b949216c6d277d5bdb986fd68a9da3
abdellaheltrach <abdellah.eltrach@gmail.com>
2026-02-06 23:29:38 +0100

    add testing project

---
+++ School.XUnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace School.XUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrenge
+            //act
+            //assert
+        }
+    }
+}
