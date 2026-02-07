48c03ffe2be70f00b5211b52cf8d40e22d403111
abdellaheltrach <abdellah.eltrach@gmail.com>
2026-02-07 11:57:32 +0100

    add VerifyToken endpoint

--- School.Api/Controllers/AuthenticationController.cs
+++ School.Api/Controllers/AuthenticationController.cs
@@ -2,9 +2,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using School.Api.Base;
+using School.Core.Base.ApiResponse;
 using School.Core.Features.Authentication.Commands.Models;
 using School.Core.Features.Authentication.Queries.Models;
 using School.Domain.AppRoutes;
+using School.Domain.Helpers;
+using Serilog;
+using System.Net;
+using System.Security.Claims;
 
 namespace School.Api.Controllers
 {
@@ -63,5 +68,52 @@ namespace School.Api.Controllers
             var response = await _mediator.Send(command);
             return NewResult(response);
         }
+
+        [HttpGet("verify")]
+        [Authorize]  // validate tokken
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status401Unauthorized)]
+        public IActionResult VerifyToken()
+        {
+            try
+            {
+
+                // Extract user information from token claims
+                var userId = User.FindFirst(nameof(UserClaimModel.Id))?.Value;
+                var username = User.FindFirst(nameof(UserClaimModel.UserName))?.Value;
+                var email = User.FindFirst(nameof(UserClaimModel.Email))?.Value;
+                var roles = User.FindAll(ClaimTypes.Role).Select(c => c.Value).ToList();
+
+                Log.Information("Token verified for user: {UserId} ({Username})", userId, username);
+
+                // Return user information
+                var userData = new
+                {
+                    userId,
+                    username,
+                    email,
+                    roles
+                };
+
+                return NewResult(new ApiResponse<object>
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Data = userData,
+                    Succeeded = true,
+                    Message = "Token is valid"
+                });
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error verifying token");
+                return NewResult(new ApiResponse<object>
+                {
+                    StatusCode = HttpStatusCode.Unauthorized,
+                    Succeeded = false,
+                    Message = "Token verification failed"
+                });
+            }
+        }
     }
 }
+
